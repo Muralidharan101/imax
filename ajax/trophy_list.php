6a384be028e2b598e03c399476abc324ce5aa93e
Muralidharan101 <118904870+Muralidharan101@users.noreply.github.com>
2023-11-08 16:09:18 +0530

    small changes

--- ajax/trophy_list.php
+++ ajax/trophy_list.php
@@ -2,15 +2,16 @@
 require_once '../datab.php';
 $res = [];
 
-$sql = "SELECT pd.id, pd.product_name, pd.product_price, pd.product_size, pd.product_color, pd.product_desc, pd.product_category, pi.product_img, pi.img_category 
+$sql = "SELECT (pd.id) AS product_id, pd.product_name, pd.product_price, pd.product_size, pd.product_color, pd.product_desc, pd.product_category, 
+        pi.product_img, pi.img_category 
         FROM product_data AS pd
         JOIN product_images AS pi ON pd.id = pi.product_id
-        WHERE pi.img_category IN ('main', 'alt') AND pd.status = 'Active' AND pd.product_type='trophy'";
+        WHERE pi.img_category='main' AND pd.status = 'Active' AND pd.product_type='trophy'";
     
 if($result = mysqli_query($conn, $sql)) {
     $data = [];
     while($row = mysqli_fetch_assoc($result)) {
-        $data[$row['id']][] = $row;
+        $data[] = $row;
     }
     $res['data']    = $data;
     $res['status']  = 'Success';
